647c0a996ec664301d76548ec3a33897cd052c62
Michael Hartl <mikehartl17@gmail.com>
2015-01-15 22:04:12 +0100

    stop catting model.lp because it is too big.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-tar cz --exclude=ins_visual --exclude=Visualizer * | ssh alg0828656@eowyn.ads.tuwien.ac.at -C "tar xz && make && ./tcbvrp && echo && echo Model: && echo && cat model.lp"
+tar cz --exclude=ins_visual --exclude=Visualizer * | ssh alg0828656@eowyn.ads.tuwien.ac.at -C "tar xz && make && ./tcbvrp $*" #  && echo && echo Model: && echo && cat model.lp"
