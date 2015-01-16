7631294a9a9bdbc3ed0eb561bce3ce179dca9925
Michael Hartl <hartl@geoinfo.tuwien.ac.at>
2015-01-16 13:36:22 +0100

    complete basic solution (subtours still allowed)

--- tcbvrp_ILP.cpp
+++ tcbvrp_ILP.cpp
@@ -67,18 +67,33 @@ void tcbvrp_ILP::solve()
 
             for (u_int k=0; k<m; k++)
             {
+	      int tourCost = 0;
+	      
                 outfile << " *** Tour *** " << k << ":" << endl;
+
+		outfile << "   ";
+		for (u_int i=0; i<n; i++)
+		  {
+		    outfile << setw(2) << i << " ";
+		  }
+		outfile << endl;
 
                 for (u_int i=0; i<n; i++)
                 {
-                    for (u_int j=0; j<n; j++)
+		  outfile << setw(2) << i << " ";
+
+		  for (u_int j=0; j<n; j++)
                     {
-                        outfile << values[index3(i,j,k)] << " ";
+		      int value = values[index3(i,j,k)] * instance.getDistance(i,j);
+
+		      tourCost += value;
+
+		      outfile << setw(2) << (value == -0 ? 0 : value) << " ";
                     }
                     outfile << endl;
                 }
 
-                outfile << endl << endl;
+                outfile << endl << "Tour costs: " << tourCost << endl << endl;
             }
 
             outfile << "Costs: " << cplex.getObjValue() << endl;
@@ -135,45 +150,49 @@ void tcbvrp_ILP::modelSCF()
         for (u_int j=0; j<n; j++)
         {
 
-	  // if this is not an allowed arc, then we will just give 0 as a maximum
-	  // var value
-	  
-	  // how UGLY! go replace it with something nicer...!
-	  int iSupply = instance.isSupplyNode(i+1);
-	  int iDemand = instance.isDemandNode(i+1);
-	  int iDepot  = (iSupply + iDemand) == 0 ? 1 : 0;
-	  int jSupply = instance.isSupplyNode(j+1);
-	  int jDemand = instance.isDemandNode(j+1);
-	  int jDepot  = (jSupply + jDemand) == 0 ? 1 : 0;
+            // if this is not an allowed arc, then we will just give 0 as a maximum
+            // var value
 
-	  int max;
-	  
-	  switch((iSupply << 5) + (iDemand << 4) + (iDepot << 3) + 
-		 (jSupply << 2) + (jDemand << 1) + jDepot) 
-	    {
-	    case (1 << 5) + (1 << 1): // supply -> demand
-	      max = 1; break;
-	    case (1 << 4) + (1 << 2): // demand -> supply
-	      max = 1; break;
-	    case (1 << 4) + 1:        // demand -> depot
-	      max = 1; break;
-	    case (1 << 3) + (1 << 2): // depot -> supply
-	      max = 1; break;
-	    default:
-	      max = 0;	     
-	    }
+            // how UGLY! go replace it with something nicer...!
+            int iSupply = instance.isSupplyNode(i);
+            int iDemand = instance.isDemandNode(i);
+            int iDepot  = (iSupply + iDemand) == 0 ? 1 : 0;
+            int jSupply = instance.isSupplyNode(j);
+            int jDemand = instance.isDemandNode(j);
+            int jDepot  = (jSupply + jDemand) == 0 ? 1 : 0;
 
-	  for (u_int k=0; k<m; k++)
+            int max;
+
+            switch((iSupply << 5) + (iDemand << 4) + (iDepot << 3) +
+                    (jSupply << 2) + (jDemand << 1) + jDepot)
+            {
+            case (1 << 5) + (1 << 1): // supply -> demand
+                max = 1;
+                break;
+            case (1 << 4) + (1 << 2): // demand -> supply
+                max = 1;
+                break;
+            case (1 << 4) + 1:        // demand -> depot
+                max = 1;
+                break;
+            case (1 << 3) + (1 << 2): // depot -> supply
+                max = 1;
+                break;
+            default:
+                max = 0;
+            }
+
+            for (u_int k=0; k<m; k++)
             {
                 //	      stringstream flowname;
-                //	      flowname << "f_" << i+1 << "_" << j+1 << "_" << k+1;
+                //	      flowname << "f_" << i << "_" << j << "_" << k;
                 stringstream xname;
-                xname << "x_" << i+1 << "_" << j+1 << "_" << k+1;
+                xname << "x_" << i << "_" << j << "_" << k;
 
-                //	      flow[index3(i,j,k)] = IloIntVar(env, 0, n-1, flowname.str().c_str());
+                //	      flow[index3(i,j,k)] = IloIntVar(env, 0, n, flowname.str().c_str());
 
-		
-           x[index3(i,j,k)]= IloIntVar(env, 0, max, xname.str().c_str());
+
+                x[index3(i,j,k)]= IloIntVar(env, 0, max, xname.str().c_str());
 
                 totalCosts += (x[index3(i,j,k)] * instance.getDistance(i,j));
             }
@@ -194,35 +213,82 @@ void tcbvrp_ILP::modelSCF()
             iter != instance.endDemandNodes();
             ++iter)
     {
-        eachDemandOnce[*iter-1] = IloExpr(env);
+        eachDemandOnce[*iter] = IloExpr(env);
         for(u_int i=0; i<n; ++i)
         {
             for(u_int k=0; k<m; ++k)
             {
-                eachDemandOnce[*iter-1] += x[index3(i,*iter-1,k)];
+                eachDemandOnce[*iter] += x[index3(i,*iter,k)];
             }
         }
-        model.add(eachDemandOnce[*iter-1] == 1);
+        model.add(eachDemandOnce[*iter] == 1);
     }
+
+    // also visit the depot once each tour
+    for(u_int k=0; k<m; ++k)
+      {
+	IloExpr visitDepotOnce(env);
+	for(u_int i=0; i<n; ++i)
+	  {
+	    visitDepotOnce += x[index3(i,0,k)];
+	  }
+	model.add(visitDepotOnce == 1);
+      }
 
     // Sum_im x_mij <= 1 forall j in S
     //   i.e. each supply node is visited at most once
 
-    IloArray<IloExpr> eachSupplyOnce(env, n);
+    IloArray<IloExpr> eachSupplyAtMostOnce(env, n);
 
     for(vector<int>::const_iterator iter = instance.beginSupplyNodes();
             iter != instance.endSupplyNodes();
             ++iter)
     {
-        eachSupplyOnce[*iter-1] = IloExpr(env);
+        eachSupplyAtMostOnce[*iter] = IloExpr(env);
         for(u_int i=0; i<n; ++i)
         {
             for(u_int k=0; k<m; ++k)
             {
-                eachSupplyOnce[*iter-1] += x[index3(i,*iter-1,k)];
+                eachSupplyAtMostOnce[*iter] += x[index3(i,*iter,k)];
             }
         }
-        model.add(eachSupplyOnce[*iter-1] <= 1);
+        model.add(eachSupplyAtMostOnce[*iter] <= 1);
+    }
+
+    // Sum_x_mij = Sum_x_mjk forall mj
+    
+    for(u_int j=0; j<n; ++j) 
+      {
+	for(u_int k=0; k<m; ++k)
+	  {
+	    IloExpr incomingConnections(env);
+
+	    for(u_int i=0; i<n; ++i)
+	      {
+		incomingConnections += x[index3(i,j,k)];
+	      }
+
+	    IloExpr outgoingConnections(env);	
+	    
+	    for(u_int l=0; l<n; ++l)
+	      {
+		outgoingConnections += x[index3(j,l,k)];
+	      }
+
+	    model.add(incomingConnections == outgoingConnections);	
+	  }
+      }
+   
+    // Sum_x_mij <= t forall m
+    //  i.e. restrict length of each subtour
+    for(u_int k=0; k<m; ++k) {
+      IloExpr tourLength(env);
+      for(u_int i=0; i<n; ++i) {
+	for(u_int j=0; j<n; ++j) {
+	  tourLength += x[index3(i,j,k)];
+	}
+      }
+      model.add(tourLength <= (int)T);	
     }
 
 }
