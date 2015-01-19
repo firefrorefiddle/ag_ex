bf524e2ff30aaa87d125474151ca019fcab75297
Michael Hartl <hartl@geoinfo.tuwien.ac.at>
2015-01-19 10:48:51 +0100

    support different servers

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-tar cz --exclude=ins_visual --exclude=Visualizer * | ssh alg0828656@eowyn.ads.tuwien.ac.at -C "tar xz && make && ./tcbvrp $*" #  && echo && echo Model: && echo && cat model.lp"
+SERVER=behemoth
+
+tar cz --exclude=ins_visual --exclude=Visualizer * | ssh "alg0828656@${SERVER}.ads.tuwien.ac.at" -C "tar xz && make && ./tcbvrp $*" #  && echo && echo Model: && echo && cat model.lp"
